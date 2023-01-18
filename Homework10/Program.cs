c34b568b73c894789f7cfb7ed8f651ee0366e631
Anna Aard <anna.mar00722@gmail.com>
2023-01-18 17:47:00 +0600

    hw10/13

---
+++ Homework10/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// 456 -> 5
+// 782 -> 8
+// // 918 -> 1
+
+int GetNumber (int num)
+{
+    int numberLast = num / 10;
+    int numberSecond = numberLast % 10;
+    return numberSecond;
+}
+
+Console.WriteLine("Введите трёхзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int second = GetNumber(number);
+
+if (number<1000 && number>99 || number>-1000 && number<-99)
+{
+    if (second <0)
+    {
+    second = second*-1;
+    }
+    Console.WriteLine ($"Второе число {number} => {second}");
+}
+else
+{
+    Console.WriteLine ($"Число не соответствует");
+}
---
+++ Homework13 var2/Program.cs
@@ -0,0 +1,23 @@
+﻿int GetNumber(int num)
+{
+    while (num > 999 || num <-999)
+    {
+        num = num / 10;
+    }
+    return num % 10;
+}
+
+int number = new Random().Next(-99999, 99999);
+int result = GetNumber(number);
+if (number > 99 || number < -99)
+{
+    if (result < 0)
+    {
+        result = result * -1;
+    }
+    Console.WriteLine($"Случайное число {number}, третья цифра {result}");
+}
+else
+{
+    Console.WriteLine($"В числе {number} третьей цифры нет");
+}
