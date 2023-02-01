9f091439b557b37a8e082dbbb84f9586072eed14
Anna Aard <anna.mar00722@gmail.com>
2023-02-02 00:10:05 +0600

    homework

---
+++ Homework41/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// -1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите количество цифр в массиве");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+int[] arrayUser = new int[sizeArray];
+UserFillArray(arrayUser);
+PrintArray(arrayUser);
+int countPositive = CountPositive(arrayUser);
+Console.WriteLine($"В данном массиве положительных чисел: {countPositive}");
+
+
+int[] UserFillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите число под индексом {i} ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($" {array[i]},");
+        else Console.Write($" {array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int CountPositive(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
---
+++ Homework43/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите координаты  первой точки прямой K");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты  второй точки прямой K");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты  первой точки прямой B");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты  второй точки прямой B");
+double b2 = Convert.ToDouble(Console.ReadLine());
+if (k1 * b2 - k2 * b1 == 0) //Проверка на параллельность
+{
+    Console.WriteLine("Заданные прямые не пересекаются");
+}
+else
+{
+    Console.WriteLine($"Координаты точки пересечения -> ");
+double[] arrayResult = new double[2];
+PointsCoordinates(k1, k2, b1, b2);
+PrintPoint(arrayResult);
+
+
+double[] PointsCoordinates(double x1, double x2, double y1, double y2)
+{
+    arrayResult[0] = Math.Round((y2 - y1) / (x1 - x2),2);
+    arrayResult[1] = Math.Round((x1 * arrayResult[0] + y1),2);
+    return arrayResult;
+}
+
+void PrintPoint(double[] arrayResult)
+{
+    Console.Write("[");
+    for (int i = 0; i < arrayResult.Length; i++)
+    {
+        if (i < arrayResult.Length - 1) Console.Write($" {arrayResult[i]},");
+        else Console.Write($" {arrayResult[i]}");
+    }
+    Console.WriteLine("]");
+}
+}
