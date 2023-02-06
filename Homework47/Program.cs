8b5ff5a0c7a15000ad7cba7763f41579948953fc
Anna Aard <anna.mar00722@gmail.com>
2023-02-06 15:19:12 +0600

    homework

---
+++ Homework47/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+//заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] CreateMatrixRandomDouble(int rows, int columns, int min, int max)
+{
+    double[,] matrix = new double[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round((rnd.NextDouble() * (max - min) + min), 2);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"|{matrix[i, j],6}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+// Не поняла из задания пользователь задаёт размер массива или я сама.
+// если пользователь, то решение ниже, если мне самой надо было, то вот.
+//double[,] arr = CreateMatrixRandomDouble(3,4, 1, 10);
+//PrintMatrix(arr);
+
+Console.WriteLine("Введите количество строк массива");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов массива");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+double[,] matrix2D = CreateMatrixRandomDouble(m, n, 1, 10);
+PrintMatrix(matrix2D);
---
+++ Homework50/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента 
+//в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого элемента в массиве нет
+
+int[,] CreateMatrixRandom(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3} | ");
+            else Console.Write($"{matrix[i, j],3} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+bool ParameterExaminated(int[,] matrix, int findRow, int findColumn)
+{
+    return (findRow < matrix.GetLength(0) && findRow > 0) && (findColumn < matrix.GetLength(1) && findColumn > 0);
+}
+
+int MatrixFindNumber(int[,] matrix, int findRow, int findColumn)
+{
+    return matrix[findRow, findColumn];
+}
+
+
+int[,] matrix2D = CreateMatrixRandom(3, 4, 1, 15);
+PrintMatrix(matrix2D);
+Console.WriteLine();
+
+Console.WriteLine("Введите строку для поиска");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите столбец для поиска");
+int columns = Convert.ToInt32(Console.ReadLine());
+if (ParameterExaminated(matrix2D, row, columns) == true)
+{
+    int findValue = MatrixFindNumber(matrix2D, row, columns);
+    Console.WriteLine($"По заданным координатам находится значение {findValue}");
+}
+else
+    Console.WriteLine($"По заданным координатам значение не найдено");
